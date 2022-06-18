7d0084161039086fc527f9a086f053360089c6e8
benl82 <benjaminliu@g.ucla.edu>
2022-06-18 02:33:20 -0700

    a few implementations of stated functions

--- RPToOsu.R
+++ RPToOsu.R
@@ -5,7 +5,7 @@ library(tidyverse)
 setwd(dirname(parent.frame(2)$ofile))
 
 getosu <- function(rp, start, bpm, snap, keys = 4, path = "getosu.txt") {
-  # rp - the list of inputs. See the other .R files in darkosu for information on their output format.
+  # rp - the df of inputs. See the other .R files in darkosu for information on their output format.
   # start - the timestamp of the start of the pattern (in ms).
   # bpm - the bpm of the map.
   # snap - the snap used. For 1/6 input 6, 1/4 input 4, 1/8 input 8, etc.
@@ -21,17 +21,26 @@ getosu <- function(rp, start, bpm, snap, keys = 4, path = "getosu.txt") {
   offs <- 60000 / (bpm * snap)
   it <- 0
   curr <- start
-  for (i in rp) {
-    curr <- floor(start + it * offs)
-    for (j in i) {
-      xbar <- c(xbar, x[j])
-      tbar <- c(tbar, curr)
-    }
-    it <- it + 1
+  for (i in 1:nrow(rp)) {
+    xbar <- c(xbar, x[rp$c[i]])
+    tbar <- c(tbar, floor(start + rp$t[i] * offs))
   }
   ybar <- rep(192, length(xbar))
   acc <- rep("1,0,0:0:0:0:", length(xbar))
   output <- data.frame(cbind(xbar, ybar, tbar, acc))
   write.table(output, file = path, sep = ",", col.names = FALSE, row.names = FALSE, quote = FALSE)
   return("hi console =D")
+}
+
+listtorp <- function(w) {
+  # Converts deprecated list format to RP
+  cbar <- c()
+  tbar <- c()
+  for (i in 1:length(w)) {
+    for (j in w[[i]]) {
+      cbar <- c(cbar, j)
+      tbar <- c(tbar, i - 1)
+    }
+  }
+  return(data.frame(cbind('c' = cbar, 't' = tbar)))
 }
--- speedgen.R
+++ speedgen.R
@@ -1,10 +1,13 @@
 source("RPToOsu.R")
+source("utility.R")
 
 # Version 1.
 # Not in .osu format.
 # In version 1 here we're not considering subsets of jumps to violate principle 2. This is because I hate coding.
 # We also don't really have a switcher or balancer so the map might suck, FYI.
 # Time complexity is also massive, don't try to make size 10^8 maps or whatever.
+
+set.seed(12345)
 
 rd <- function(x, n) {
   # A new random function, since sample() has unintended side effects.
@@ -77,7 +80,7 @@ p2 <- function(x, isj, j) {
   return(s[which(!s %in% r)])
 }
 gen1 <- function(n = 100, j = 12) {
-  # n: The length (number of sub-beats)
+  # n: The length (number of sub-beats) + 1
   # j: Jump every j beats. Considered jumpstream if j <= 4, so don't put any values less than 5 for j.
   # Enter j = 0 (or any negative number) if you don't want any jumps.
   # Starts with a jump by default.
@@ -87,7 +90,13 @@ gen1 <- function(n = 100, j = 12) {
   } else {
     w[[1]] <- rd(1:4, 2)
   }
-  for (i in 2:n) {
+  w <- vector(mode = "list", length = n)
+  if (j <= 0) {
+    w[[1]] <- rd(1:4, 1)
+  } else {
+    w[[1]] <- rd(1:4, 2)
+  }
+  for (i in 2:(n+1)) {
     if (i > 2) {
       s1 <- p1(w[(i-2):(i-1)])
     } else {
@@ -101,8 +110,18 @@ gen1 <- function(n = 100, j = 12) {
     s <- s1[which(s1 %in% s2)]
     w[[i]] <- rd(s, isj)
   }
-  return(w)
+  return(listtorp(w))
 }
 
 # Just a test, this is for my file "Map of Scars"
 getosu(rp = gen1(n = 1000, j = 12), start = 316783, bpm = 115, snap = 12, keys = 4)
+
+df <- gen1()
+head(df)
+a <- point(df, 0)
+b <- point(df, 1)
+
+v1 <- c(1,2,3,4,1,2,3,2,3,4,1,2,4)
+v2 <- c(0,0,0,0,1,1,1,2,2,2,3,3,3)
+rpt <- data.frame(cbind('c' = v1, 't' = v2))
+jackdensity(rpt)
