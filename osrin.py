532c2b81813561f0273b6d9926c24ffab64cf8f6
benl82 <benjaminliu@g.ucla.edu>
2022-06-21 06:01:23 -0700

    added lists to osrinpy

--- osrin.py
+++ osrin.py
@@ -20,6 +20,23 @@ timestamp = timestamp.replace(tzinfo=timezone.utc)
 rdatabounds = list(map(int, str[16][13:].split(' ')))
 rid = str[17][11:]
 
-pathf = "replay-mania_1443309_419638875.osr";
+pathf = "replay-mania_3362200_497162661.osr";
 with open(pathf, "rb") as f:
-    data = f.read();
+    data = f.read()
+    data = data[rdatabounds[0]:rdatabounds[1]]
+    data = lzma.decompress(data, format=lzma.FORMAT_AUTO).decode("ascii")
+    data = data[:-1]
+    arr = [i.split('|') for i in data.split(',')]
+    rng = None 
+    arrdelta = []
+    arrx = []
+    for i in arr:
+        delta = int(i[0])
+        x = i[1]
+        if delta == -12345 and i == arr[-1]:
+            rng = int(i[3])
+            continue
+        arrdelta.append(delta)
+        arrx.append(x)
+
+# come back to this
